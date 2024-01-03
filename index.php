738bda50121638970cad3f9b1f2e1a43b514d92f
A. Rodríguez <antonio@arodriguez.eu>
2024-01-03 17:52:35 +0100

    Se añade formato y se guarda

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <html>
 <head>
-<title></title>
+    <meta charset="utf-8">
+<title>Página de pruebas</title>
+<link href="css/estilos.css" rel="stylesheet" type="text/css">
 </head>
 <body>
+    <h1>Proyecto de pruebas</h1>
 </body>
 </html>
